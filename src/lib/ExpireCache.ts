fb82372e211e4cddaf10d13de6869439d6aecd8f
lip8up <lip8up@qq.com>
2023-05-12 10:48:29 +0800

    release v6.199

--- src/lib/ExpireCache.ts
+++ src/lib/ExpireCache.ts
@@ -19,20 +19,6 @@ export default class ExpireCache<ValueType = any> {
   }
 
   /**
-   * 清理所有过期的项目。
-   */
-  clean() {
-    const now = Date.now()
-    for (const name in this.#store) {
-      const item = this.#store[name]!
-      const span = now - item.time
-      if (span > this.#maxAge) {
-        delete this.#store[name]
-      }
-    }
-  }
-
-  /**
    * 存储项目。
    * @param name 项目名。
    * @param value 项目值。
@@ -48,9 +34,35 @@ export default class ExpireCache<ValueType = any> {
   /**
    * 获取缓存的项目，若不存在或过期，返回 undefined。
    * @param name 项目名
+   * @param maxAge 可选的 maxAge，若设置，将取代默认 maxAge
    */
-  get(name: string): ValueType | undefined {
-    this.clean()
-    return this.#store[name]?.value
+  get(name: string, maxAge?: number): ValueType | undefined {
+    const age = maxAge || this.#maxAge
+    if (name in this.#store) {
+      const item = this.#store[name]!
+      const span = Date.now() - item.time
+      if (span <= age) {
+        return item.value
+      }
+      // 删除已过期的项目
+      delete this.#store[name]
+    }
+  }
+
+  /**
+   * 清理所有过期的项目。
+   * @param maxAge 可选的 maxAge，若设置，将取代默认 maxAge，传入 0，则清空所有
+   */
+  clean(maxAge?: number) {
+    // 若 age 为 0，则作为 -1 使用，因为同步情况下，下面的 now - item.time 有可能为 0，传入 0 应该删除所有
+    const age = (maxAge ?? this.#maxAge) || -1
+    const now = Date.now()
+    for (const name in this.#store) {
+      const item = this.#store[name]!
+      const span = now - item.time
+      if (span > age) {
+        delete this.#store[name]
+      }
+    }
   }
 }
--- tests/ExpireCache.test.ts
+++ tests/ExpireCache.test.ts
@@ -7,4 +7,11 @@ test('ExpireCache', async () => {
   expect(cache.get('foo')).toEqual('bar')
   await delay(199)
   expect(cache.get('foo')).toBeUndefined()
+
+  const key = 'object'
+  const value = { a: 1, b: { c: 2, d: 3, e: { f: 4, g: 5, h: 6 } } }
+  cache.set(key, value)
+  expect(cache.get(key)).toEqual(value)
+  cache.clean(0)
+  expect(cache.get(key)).toBeUndefined()
 })
